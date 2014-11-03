0af72e55760cf10101fe2f4fe785954aaf9a4ada
cugone <cugone@gmail.com>
2014-11-03 17:11:54 -0600

    CableForceGenerator physics
    Fixed CableForceGenerator physics implementation in Update.

--- Allegro2DEngine/Engine/Physics/ForceGenerators/CCableForceGenerator.cpp
+++ Allegro2DEngine/Engine/Physics/ForceGenerators/CCableForceGenerator.cpp
@@ -13,30 +13,36 @@ void CableForceGenerator::Update(double /*deltaTime*/) {
     a2de::RigidBody* sb = _cable_ends.second->GetBody();
     if(sb == nullptr) return;
 
-    double square_distance = (fb->GetPosition() - sb->GetPosition()).GetLengthSquared();
+    a2de::Vector2D fbp = fb->GetPosition();
+    a2de::Vector2D sbp = sb->GetPosition();
+
+    double square_distance = (fbp - sbp).GetLengthSquared();
     double square_length = _length * _length;
 
     if(square_distance < square_length || a2de::Math::IsEqual(square_length, square_distance)) return;
 
-    a2de::Vector2D direction_away_from_sb = (fb->GetPosition() - sb->GetPosition()).Normalize();
-    a2de::Vector2D direction_away_from_fb = (sb->GetPosition() - fb->GetPosition()).Normalize();
-
-    double m1 = fb->GetMass();
-    double m2 = sb->GetMass();
-    double mass_sum = m1 + m2;
-
-    double m1_ratio = m1 / mass_sum;
-    double m2_ratio = m2 / mass_sum;
-    
-    
-    a2de::Vector2D m1force;
-    a2de::Vector2D m2force;
     if(square_length < square_distance) {
+
+        a2de::Vector2D sb_to_center = (fbp - sbp).Normalize();
+        a2de::Vector2D fb_to_center = (sbp - fbp).Normalize();
+
+        double m1 = fb->GetMass();
+        double m2 = sb->GetMass();
+        double mass_sum = m1 + m2;
+
+        double m1_ratio = m1 / mass_sum;
+        double m2_ratio = m2 / mass_sum;    
+        
+        a2de::Vector2D m1force = a2de::Vector2D::GetProjection(m1 * fb->GetAcceleration(), fb_to_center);
+        a2de::Vector2D m2force = a2de::Vector2D::GetProjection(m2 * sb->GetAcceleration(), sb_to_center);
+
         double distance = std::sqrt(square_distance);
-        //double move_distance = distance - _length;
-        double distance_ratio = distance / _length;
-        fb->ApplyForce(direction_away_from_fb * distance_ratio * m1_ratio, 0.0);
-        sb->ApplyForce(direction_away_from_sb * distance_ratio * m2_ratio, 0.0);
+        double move_distance = distance - _length;
+
+        fb->ApplyImpulse(m1force);
+        sb->ApplyImpulse(m2force);
+        sb->SetPosition(sbp + ((sb_to_center * move_distance * m2_ratio)));
+        fb->SetPosition(fbp + ((fb_to_center * move_distance * m1_ratio)));
     }
 
 }
